76b10ece4f82da414328534103b792b70e8c75f5
leekli <li.kirkham@live.com>
2022-12-01 09:59:09 +0000

    adds day 1 part 2 solution

--- solutions/day1/day1.js
+++ solutions/day1/day1.js
@@ -1,5 +1,8 @@
 const day1Input = require("./input");
 
+// AOC - DAY 1 🎄
+
+// Part 1
 function calorieCounting(calories) {
   const splitCalories = calories.split("\n");
   let currentElfCalorieCount = 0;
@@ -18,4 +21,27 @@ function calorieCounting(calories) {
   return highestCount;
 }
 
+// Part 2
+function calorieCountingTop3(calories) {
+  const splitCalories = calories.split("\n");
+  let currentElfCalorieCount = 0;
+  let totalCount = 0;
+  const calorieSumsArray = [];
+
+  splitCalories.forEach((calorieSet, index) => {
+    if (calorieSet !== "") currentElfCalorieCount += Number(calorieSet);
+    else if (calorieSet === "" || index === splitCalories.length - 1) {
+      calorieSumsArray.push(currentElfCalorieCount);
+      currentElfCalorieCount = 0;
+    }
+  });
+
+  const sortedSumsArray = calorieSumsArray.sort((a, b) => b - a);
+
+  totalCount = sortedSumsArray[0] + sortedSumsArray[1] + sortedSumsArray[2];
+
+  return totalCount;
+}
+
 console.log(calorieCounting(day1Input));
+console.log(calorieCountingTop3(day1Input));
