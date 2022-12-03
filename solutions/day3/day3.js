cde8a9ff201e2d437a2a25c13bc51f6cc972f9ca
leekli <li.kirkham@live.com>
2022-12-03 13:32:34 +0000

    adds day 3 part 2 solution

--- solutions/day3/day3.js
+++ solutions/day3/day3.js
@@ -31,4 +31,37 @@ function ruckSackReOrg(rucksackList) {
   return prioritiesTotalSum;
 }
 
-console.log(ruckSackReOrg(day3Input));
+// Part 2
+function mostCommonBagdes(rucksackList) {
+  const splitRuckSackList = rucksackList.split("\n");
+  let sumTotal = 0;
+
+  const splitRuckSacksBy3 = 3;
+  for (let i = 0; i < splitRuckSackList.length; i += splitRuckSacksBy3) {
+    const split = splitRuckSackList.slice(i, i + splitRuckSacksBy3);
+    let mostCommonLetter = "";
+    for (let j = 0; j < split.length; j++) {
+      let allCharsIn3Rucksacks = [
+        ...new Set(split[0] + split[1] + split[2]),
+      ].join("");
+
+      for (let char of allCharsIn3Rucksacks) {
+        if (
+          split[0].includes(char) &&
+          split[1].includes(char) &&
+          split[2].includes(char)
+        ) {
+          mostCommonLetter = char;
+        }
+      }
+    }
+    let letterCharCode =
+      mostCommonLetter.charCodeAt(0) -
+      (mostCommonLetter === mostCommonLetter.toUpperCase() ? 38 : 96);
+    sumTotal += letterCharCode;
+  }
+  return sumTotal;
+}
+
+console.log(ruckSackReOrg(day3Input), "Part 1 answer");
+console.log(mostCommonBagdes(day3Input), "Part 2 answer");
