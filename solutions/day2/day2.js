5cb85e542feb41d50efd5cea8fb57d7c5290ab87
leekli <li.kirkham@live.com>
2022-12-03 13:50:37 +0000

    refactored day 2 solution

--- solutions/day2/day2.js
+++ solutions/day2/day2.js
@@ -5,38 +5,25 @@ const day2Input = require("./input.js");
 // Part 1
 function rockPaperScissors(gameGuide) {
   const splitGameGuide = gameGuide.toUpperCase().split("\n");
-  const splitGameGuideLength = splitGameGuide.length;
   let totalScore = 0;
-  let currentMoveSelectedScore = 0;
-  let currentRoundScore = 0;
 
-  const moveSelectedScoresLookup = {
-    X: 1,
-    Y: 2,
-    Z: 3,
+  const scoreTableLookup = {
+    "A X": 4,
+    "B Y": 5,
+    "C Z": 6,
+    "C X": 7,
+    "B Z": 9,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "C Y": 2,
   };
 
-  for (let i = 0; i < splitGameGuideLength; i++) {
-    const myMove = splitGameGuide[i][2];
-    currentMoveSelectedScore = moveSelectedScoresLookup[myMove];
-    if (
-      splitGameGuide[i] === "A X" ||
-      splitGameGuide[i] === "B Y" ||
-      splitGameGuide[i] === "C Z"
-    ) {
-      currentRoundScore = 3;
-    } else if (
-      splitGameGuide[i] === "C X" ||
-      splitGameGuide[i] === "B Z" ||
-      splitGameGuide[i] === "A Y"
-    ) {
-      currentRoundScore = 6;
-    } else {
-      currentRoundScore = 0;
+  splitGameGuide.forEach((game) => {
+    for (let move in scoreTableLookup) {
+      if (game === move) totalScore += scoreTableLookup[move];
     }
-
-    totalScore += currentMoveSelectedScore + currentRoundScore;
-  }
+  });
 
   return totalScore;
 }
@@ -44,55 +31,25 @@ function rockPaperScissors(gameGuide) {
 // Part 2
 function rockPaperScissorsDecoded(gameGuide) {
   const splitGameGuide = gameGuide.toUpperCase().split("\n");
-  const splitGameGuideLength = splitGameGuide.length;
   let totalScore = 0;
-  let currentMoveSelectedScore = 0;
-  let currentRoundScore = 0;
 
-  const lookupObject = {
-    moveSelectedScore: {
-      X: 1,
-      Y: 2,
-      Z: 3,
-    },
-    winMove: {
-      A: "Y",
-      B: "Z",
-      C: "X",
-    },
-    drawMove: {
-      A: "X",
-      B: "Y",
-      C: "Z",
-    },
-    loseMove: {
-      A: "Z",
-      B: "X",
-      C: "Y",
-    },
+  const scoreTableLookup = {
+    "A X": 3,
+    "B Y": 5,
+    "C Z": 7,
+    "C X": 2,
+    "B Z": 9,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "C Y": 6,
   };
 
-  for (let i = 0; i < splitGameGuideLength; i++) {
-    const opponentMove = splitGameGuide[i][0];
-    const myMove = splitGameGuide[i][2];
-    if (myMove === "X") {
-      const move = lookupObject.loseMove[opponentMove];
-      currentMoveSelectedScore = lookupObject.moveSelectedScore[move];
-      currentRoundScore = 0;
+  splitGameGuide.forEach((game) => {
+    for (let move in scoreTableLookup) {
+      if (game === move) totalScore += scoreTableLookup[move];
     }
-    if (myMove === "Y") {
-      const move = lookupObject.drawMove[opponentMove];
-      currentMoveSelectedScore = lookupObject.moveSelectedScore[move];
-      currentRoundScore = 3;
-    }
-    if (myMove === "Z") {
-      const move = lookupObject.winMove[opponentMove];
-      currentMoveSelectedScore = lookupObject.moveSelectedScore[move];
-      currentRoundScore = 6;
-    }
-
-    totalScore += currentRoundScore + currentMoveSelectedScore;
-  }
+  });
 
   return totalScore;
 }
